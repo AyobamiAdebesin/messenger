6121ff593b70febf9c13bfee718342f3a9fd1792
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-17 09:54:53 +0100

    router for third party

--- routers/thirdPartyRouter.ts
+++ routers/thirdPartyRouter.ts
@@ -1,6 +1,13 @@
 import express from "express";
 import ThirdPartyController from "../controllers/thirdPartyController";
+import thirdPartyRequired from "../middleware/thirdpartyAuthMiddleware";
 
 const router: express.Router = express.Router();
 
+router.get("/orders", thirdPartyRequired, ThirdPartyController.getAllOrdersByLogistics);
+router.get("/orders/:orderId", thirdPartyRequired, ThirdPartyController.getSingleOrder);
+router.get("/orders", thirdPartyRequired, ThirdPartyController.getOrderByStatus);
+
 //router.get("/get_all", ThirdPartyController.getAllThirdParties);
+
+module.exports = router;
