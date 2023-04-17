28c52cc58019adcbf7e495b2acddceb4a3bc40fe
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-17 03:14:01 +0100

    Created  thrid party router

---
+++ routers/thirdPartyRouter.ts
@@ -0,0 +1,6 @@
+import express from "express";
+import ThirdPartyController from "../controllers/thirdPartyController";
+
+const router: express.Router = express.Router();
+
+//router.get("/get_all", ThirdPartyController.getAllThirdParties);
