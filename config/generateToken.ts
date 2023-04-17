065d89644ad9280f4c95502e0f08604edfa8911c
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-17 01:55:18 +0100

    Annotated function parameter. This is JavaScript on steroids!

--- config/generateToken.ts
+++ config/generateToken.ts
@@ -1,7 +1,7 @@
 // Function to generate a token for the user
 const jwt = require("jsonwebtoken");
 
-export const generateToken = (id : any) => {
+export const generateToken = (id : string) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
     expiresIn: "3d",
   });
