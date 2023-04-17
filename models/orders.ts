299a1e5647d807ce400df8c4d529dd9a68d7d248
AyobamiAdebesin <froshofficial@gmail.com>
2023-04-17 02:03:13 +0100

    Extend interface with Document interface from mongoose. Renamed enum values

--- models/orders.ts
+++ models/orders.ts
@@ -3,18 +3,18 @@
  * @description Defines the Order Model
  * @author Ayobami Adebesin
  */
-import { Schema, model, Types } from "mongoose";
+import { Schema, model, Types, Document } from "mongoose";
 // Enum for Order status
 export enum OrderStatus {
-  Pending = "pending",
-  InProgress = "in_progress",
-  Delivered = "delivered",
-  Cancelled = "cancelled",
+  Pending = "PENDING",
+  InProgress = "IN_PROGRESS",
+  Delivered = "DELIVERED",
+  Cancelled = "CANCELLED",
 }
 
 // Create interface representing an Order document
-export interface IOrder {
-  id: string;
+export interface IOrder extends Document{
+  _id: string;
   order_description: string;
   customer_id: Types.ObjectId;
   rider_id: Types.ObjectId;
